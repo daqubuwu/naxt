9a1cabb39b4562fe54cbf7b1a9fc5638273d24c1
daqubuwu <daqubeen@gmail.com>
2024-10-03 09:32:24 +0900

    themes/anim

--- src/app/Components/Footer/Footer.jsx
+++ src/app/Components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='flex justify-between px-14 pb-8 text-sm pt-32'>
-            <p className='footer-mail'>DAQUBEEN@VANGOGH.COM</p>
+            <p className='footer-mail footer-item'>DAQUBEEN@VANGOGH.COM</p>
             <div className='footer-smedia flex gap-7 justify-between'>
-                <p>DRIBBLE</p>
-                <p>MYSPACE</p>
-                <p>GITHUB</p>
-                <p>INSTAGRAM</p>
+                <p className='footer-item'>DRIBBLE</p>
+                <p className='footer-item'>MYSPACE</p>
+                <p className='footer-item'>GITHUB</p>
+                <p className='footer-item'>INSTAGRAM</p>
             </div>
         </footer>
     );
